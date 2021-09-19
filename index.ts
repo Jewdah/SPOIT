150035938c38aad02da6628be79db8154c09fea2
David Gamba <dgambami@uninpahu.edu.co>
2021-09-19 11:59:51 -0500

    subida test3

--- index.ts
+++ index.ts
@@ -1,7 +1,11 @@
-import{resultado} from "./modulo1";
-import{message1,resultadoOp,vuelo1,minusculas,mayusculas,message2,example1,reemplazo,tipo,espacios,subcadena2,buscando1} from "./modulo2"
+import{ResultadoFinal} from "./modulo1";
+import{message1,resultadoOp,vuelo1,minusculas,mayusculas,message2,example1,reemplazo,tipo,espacios,subcadena2,buscando1} from "./modulo2";
+import{TextoResultadosOperaciones} from "./modulo3";
 
-console.log("---String");
+
+console.log(ResultadoFinal);
+
 console.log(message1,resultadoOp,vuelo1,minusculas,mayusculas,message2,example1,reemplazo,tipo,espacios,subcadena2,buscando1);
-console.log("---Numeros ");
-console.log(resultado);
+
+
+console.log(TextoResultadosOperaciones);
